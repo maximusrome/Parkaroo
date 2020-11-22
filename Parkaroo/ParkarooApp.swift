91a6ef7428c5e904767c355e926e640ec8b5b419
max rome <rome.a.max@gmail.com>
2020-11-22 15:50:27 -0500

    Initial Commit

---
+++ Parkaroo/ParkarooApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkarooApp.swift
+//  Parkaroo
+//
+//  Created by max rome on 11/22/20.
+//
+
+import SwiftUI
+
+@main
+struct ParkarooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
