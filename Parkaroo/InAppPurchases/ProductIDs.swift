645f0c83a4612059e21aba89c9bb2acef7662d72
romemax <rome.a.max@gmail.com>
2021-07-01 19:49:42 -0400

    Files

--- Parkaroo/InAppPurchases/ProductIDs.swift
+++ Parkaroo/InAppPurchases/ProductIDs.swift
@@ -19,3 +19,40 @@ enum Products: String {
         }
     }
 }
+//  @EnvironmentObject var iapManager: IAPManager
+//                        if let product = iapManager.creditProduct {
+//                            iapManager.currentPurchasingProduct = product
+//                            iapManager.purchaseProduct(product: product)
+//                        }
+//            .onChange(of: iapManager.transactionState, perform: { value in
+//                if iapManager.currentPurchasingProduct?.productIdentifier == "parkaroo.1credit"  {
+//                    switch iapManager.transactionState {
+//                    case .purchased:
+//                        iapManager.currentPurchasingProduct = nil
+//                        userInfo.addCredits(numberOfCredits: 1) { result in
+//                            switch result {
+//                            case .success(_):
+//                                print("Successful purchase")
+//                                iapManager.showActivityIndicator = false
+//                            case .failure(_):
+//                                print("Error updating credits")
+//                                iapManager.showActivityIndicator = false
+//                            }
+//                        }
+//                    case .failed:
+//                        print("Error Purchasing")
+//                        iapManager.showActivityIndicator = false
+//                        iapManager.currentPurchasingProduct = nil
+//                    case .purchasing:
+//                        iapManager.showActivityIndicator = true
+//                        print("Purchasing...")
+//                    default:
+//                        print("Other Error")
+//                        iapManager.showActivityIndicator = false
+//                        iapManager.currentPurchasingProduct = nil
+//                    }
+//                }
+//            })
+//            if iapManager.showActivityIndicator {
+//                ActivityIndicatorView()
+//            }
